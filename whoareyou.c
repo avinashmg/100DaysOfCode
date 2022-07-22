50e04ddd4f277deeb52c060f1cfe0a1c0a736bbc
Avinash M G <hatman290@gmail.com>
2022-07-22 15:07:13 +0530

    Add: whoareyou program

---
+++ whoareyou.c
@@ -0,0 +1,10 @@
+// Day 1 of 100 Days of Code challenge in C 
+#include<stdio.h>
+
+int main(){
+    char me[20];
+    printf("What is your name?");
+    scanf("%s", &me);
+    printf("Darn glad to neet you, %s!\n", me);
+    return(0);
+}
